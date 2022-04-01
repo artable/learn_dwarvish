4be0677528c5dbc8589a70568dccbb4b28089758
Benjamin Anderson <79383914+Grizzek98@users.noreply.github.com>
2022-04-01 16:53:42 -0600

    hooked up quiz2 and quiz3

--- frontend/src/components/pages/Lesson2/Quiz2.js
+++ frontend/src/components/pages/Lesson2/Quiz2.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import QuestionHandler from '../../../QuestionHandler';
 import Navbar from '../../Navbar';
 
-const Quiz2 = () => {
-    return (
-        <section className="Quiz2">
-            <Navbar />
-            <h1>This is Quiz 2</h1>
-            <Link to='/lesson3'>Go to Lesson 3</Link>
-        </section>
-    )
-}
+class Quiz2 extends Component {
+    constructor(props) {
+        super(props);
+    }
 
+    render() {
+        return (
+            <section className="Quiz2">
+                <Navbar />
+                <h1 className='header-quiz'>Quiz 2</h1>
+                <QuestionHandler />
+                <div className='link-box'>
+                    <Link className='link-quiz2' to='/lesson3'>Go to Lesson 3</Link>
+                </div>
+            </section>
+        )   
+    }
+}
 export default Quiz2;
