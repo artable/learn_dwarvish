667e96ef82ab106960ead0b82d686b0ce14b5751
Benjamin Anderson <79383914+Grizzek98@users.noreply.github.com>
2022-04-01 17:05:36 -0600

    added a little more info home, about us, and contact

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -6,6 +6,8 @@ const Contact = () => {
         <section className="Contact">
             <Navbar />
             <h1>This is the Contact page</h1>
+            <div>Probably shouldn't put sensitive information on here and we don't have a dedicated means of contact for this web app, so...</div>
+            <div>Sorry.</div>
         </section>
     );
 };
--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -6,6 +6,9 @@ const Home = () => {
     <section className="Home">
       <Navbar />
       <h1>Learn Cirth</h1>
+      <div>Welcome to Learn Cirth!</div>
+      <div>This site is split into different lessons covering various sets of the Cirth language, with accompanying quizzes to test your knowledge.</div>
+      <div>Click 'Lesson 1' in the navbar above to begin!</div>
 
     </section>
   );
